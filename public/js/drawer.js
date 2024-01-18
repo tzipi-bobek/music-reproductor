4763d1a5a208bca850695e7b946b4339cb7babbb
tzipi-bobek <tzipibobek@gmail.com>
2024-01-18 19:35:11 -0300

    se puede sacar los lyrics

--- public/js/drawer.js
+++ public/js/drawer.js
@@ -3,14 +3,12 @@
 window.addEventListener('DOMContentLoaded', (event) => {
   document.querySelectorAll('.open-btn').forEach((button) => {
     button.addEventListener('click', function () {
-      // Cierra todos los drawers y limpia su contenido
       document.querySelectorAll('.drawer').forEach((drawer) => {
         drawer.style.width = '0';
         const lyricsElement = drawer.querySelector('#lyrics');
         lyricsElement.innerHTML = '';
       });
 
-      // Abre el drawer seleccionado y actualiza su contenido
       const drawerId = this.getAttribute('data-drawer-id');
       let lyrics = this.getAttribute('data-lyrics');
       if (lyrics) {
@@ -22,7 +20,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
       const lyricsElement = drawer.querySelector('#lyrics');
       lyricsElement.innerHTML = lyrics;
 
-      // Crea un elemento temporal para calcular el ancho del texto
       const tempElement = document.createElement('div');
       tempElement.style.position = 'absolute';
       tempElement.style.visibility = 'hidden';
@@ -30,12 +27,18 @@ window.addEventListener('DOMContentLoaded', (event) => {
       tempElement.innerHTML = lyrics;
       document.body.appendChild(tempElement);
 
-      // Calcula el ancho del texto y aplica el ancho al drawer
-      const textWidth = Math.max(tempElement.getBoundingClientRect().width, 300); // Asegura un ancho mínimo de 200px
+      const textWidth = Math.max(tempElement.getBoundingClientRect().width, 300);
       drawer.style.width = `${textWidth}px`;
 
-      // Elimina el elemento temporal
       document.body.removeChild(tempElement);
+    });
+  });
+
+  document.querySelectorAll('.close-btn').forEach((button) => {
+    button.addEventListener('click', function (evento) {
+      const drawer = this.parentNode;
+      drawer.style.width = '0';
+      event.stopPropagation();
     });
   });
 });
